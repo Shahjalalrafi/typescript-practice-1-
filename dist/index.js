91ec84ed922bd4ee3aaa483b85fb7235553f752b
shahjalal rafi <shahjalalrafi5@gmail.com>
2021-02-12 21:46:11 +0300

    upload

--- dist/index.js
+++ dist/index.js
@@ -5,4 +5,12 @@ let playerName;
 playerName = 'mashrafee';
 console.log(playerName);
 let fruits = ['apple', 'mango'];
+fruits.push('orange');
 console.log(fruits);
+let person = {
+    name: 'shahjalal rafi',
+    age: 21,
+    isMaried: false
+};
+person.name = 'shahaziz rakib';
+console.log(person);
--- src/index.ts
+++ src/index.ts
@@ -7,4 +7,22 @@ playerName = 'mashrafee'
 console.log(playerName)
 
 let fruits = ['apple', 'mango']
+fruits.push('orange')
+
 console.log(fruits)
+
+let person = {
+    name: 'shahjalal rafi',
+    age: 21,
+    isMaried: false
+}
+
+person.name = 'shahaziz rakib'
+// console.log(person)
+
+type stringOrNumber = string | never
+type userType = {name: string; age :number}
+
+const userDetails = (id:stringOrNumber, user: userType) => {
+    console.log(`user id is ${id} and user name is ${user.name} and his age is ${user.age}`)
+}
