5e0b08e20843af48c9661cb863f244b89c642e0f
shahjalal rafi <shahjalalrafi5@gmail.com>
2021-02-12 20:18:47 +0300

    upload

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+// var country = 'i love bangladesh and bangladesi people'
+// console.log(country)
+let playerName;
+playerName = 'mashrafee';
+console.log(playerName);
+let fruits = ['apple', 'mango'];
+console.log(fruits);
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+// var country = 'i love bangladesh and bangladesi people'
+// console.log(country)
+
+let playerName
+playerName = 'mashrafee'
+
+console.log(playerName)
+
+let fruits = ['apple', 'mango']
+console.log(fruits)
